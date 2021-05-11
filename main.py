0fb1d1a1aa5b5d2f865d4db6c119cf2aba6f60bd
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-11 18:14:00 -0300

    add main()

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+def main():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
 
-drive = GoogleDrive(gauth)
+    drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1.SetContentString('Hello World!') # Set content of the file from given string.
-file1.Upload()
+    file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+    file1.SetContentString('Hello World!') # Set content of the file from given string.
+    file1.Upload()
+
+if __name__ == '__main__':
+    main()

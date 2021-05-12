cd67ea78189293d29df9c94cbc7c6b56f3a75f7e
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-12 20:19:24 -0300

    add test to see if it’s running locally

--- main.py
+++ main.py
@@ -159,10 +159,12 @@ def print_index_table():
 
 
 if __name__ == '__main__':
-  # When running locally, disable OAuthlib's HTTPs verification.
-  # ACTION ITEM for developers:
-  #     When running in production *do not* leave this option enabled.
-  # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  # ‘home’ is a regular (empty) file used to tell the program to run locally.
+  if os.path.exists('home'):
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.

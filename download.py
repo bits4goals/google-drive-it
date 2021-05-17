cd336af793d445cd3aac8fde2def859c49ddaec1
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-17 17:22:34 -0300

    improve function definition

--- download.py
+++ download.py
@@ -18,38 +18,37 @@ class Url:
         self.url = url
 
 
-def download_temp(url):
-    """Download URL target, save it locally as a temporary file.
-Return a tuple containing the local path where it was saved and its
-remote original file name.
+    def download(self, filename=None, tempfile=False):
+        """Fetch and persist the file from the object’s URL.
 
-Raises:
-ValueError (malformed url)
-URLError
-"""
-    try:
-        with urllib.request.urlopen(url) as response:
-            try:
-                parsed_url_path = urllib.parse.urlparse(response.url).path
-            except:
-                print(error_msg.format('Problems parsing the URL'))
-                raise
-            remote_filename = os.path.basename(parsed_url_path)
-            with tempfile.NamedTemporaryFile(delete=False) as f:
-                shutil.copyfileobj(response, f)
-            local_filepath = f.name
-    except ValueError:
-        # Raised by urllib.request.urlopen or urllib.parse.urlparse.
-        print(error_msg.format('Malformed or invalid URL'))
-        # raise
-    except urllib.error.URLError as e:
-        # Raised by urllib.request.urlopen.
-        print(error_msg.format(e.reason.strerror))
-        # raise
-    except:
-        raise Exception('Error: download_temp: Download process failed')
-    else:
-        return local_filepath, remote_filename
+        Return the path where the file was saved to.
+        If FILENAME was provided, use it to save the file, or else
+        use the name obtained from the remote server.
+        When TEMPFILE is True, create a temporary"""
+
+        try:
+            with urllib.request.urlopen(url) as response:
+                try:
+                    parsed_url_path = urllib.parse.urlparse(response.url).path
+                except:
+                    print(error_msg.format('Problems parsing the URL'))
+                    raise
+                remote_filename = os.path.basename(parsed_url_path)
+                with tempfile.NamedTemporaryFile(delete=False) as f:
+                    shutil.copyfileobj(response, f)
+                local_filepath = f.name
+        except ValueError:
+            # Raised by urllib.request.urlopen or urllib.parse.urlparse.
+            print(error_msg.format('Malformed or invalid URL'))
+            # raise
+        except urllib.error.URLError as e:
+            # Raised by urllib.request.urlopen.
+            print(error_msg.format(e.reason.strerror))
+            # raise
+        except:
+            raise Exception('Error: download_temp: Download process failed')
+        else:
+            return local_filepath, remote_filename
 
 
 download_temp('https://www.bits4wuts.com/foo.txt')

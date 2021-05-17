439d602aaa7798b13668029985477dfa79e44b93
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-17 17:01:11 -0300

    add error handling

--- download.py
+++ download.py
@@ -1,17 +1,42 @@
 import shutil
-import urllib.parse
 import urllib.request
+import urllib.parse
+import urllib.error
 import os
+
+
+error_msg = 'Error: {}'
 
 
 def download_temp(url):
     """Download URL target, save it locally as a temporary file.
 Return a tuple containing the local path where it was saved and its
-remote original file name."""
-    with urllib.request.urlopen(url) as response:
-        parsed_url_path = urllib.parse.urlparse(response.url).path
-        remote_filename = os.path.basename(parsed_url_path)
-        with tempfile.NamedTemporaryFile(delete=False) as f:
-            shutil.copyfileobj(response, f)
-        local_filepath = f.name
-    return (local_filepath, remote_filename)
+remote original file name.
+
+Raises:
+ValueError (malformed url)
+URLError
+"""
+    try:
+        with urllib.request.urlopen(url) as response:
+            try:
+                parsed_url_path = urllib.parse.urlparse(response.url).path
+            except:
+                print(error_msg.format('Problems parsing the URL'))
+                raise
+            remote_filename = os.path.basename(parsed_url_path)
+            with tempfile.NamedTemporaryFile(delete=False) as f:
+                shutil.copyfileobj(response, f)
+            local_filepath = f.name
+    except ValueError:
+        # Raised by urllib.request.urlopen or urllib.parse.urlparse.
+        print(error_msg.format('Malformed or invalid URL'))
+        # raise
+    except urllib.error.URLError as e:
+        # Raised by urllib.request.urlopen.
+        print(error_msg.format(e.reason.strerror))
+        # raise
+    except:
+        raise Exception('Error: download_temp: Download process failed')
+    else:
+        return local_filepath, remote_filename

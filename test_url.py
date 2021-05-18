b856eac5ee0d94656152b8c6cc27ce56248a822c
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-18 18:16:47 -0300

    make internal method into external function

--- test_url.py
+++ test_url.py
@@ -6,26 +6,25 @@ import string
 import itertools
 
 
+def urls_for_test():
+    """URL generator."""
+
+    protocols = ['', 'file:///', 'ftp://', 'http://', 'https://']
+    paths = ['', 'home', 'home/foo', 'home/foo/bar.txt']
+    params = ['', '?abc=cde', '?abc=cde&FgH=iJk!@#$%']
+    randoms = ['', ''.join(random.choices(string.ascii_letters, k=32))]
+
+    for comb in itertools.product(protocols, paths, params, randoms):
+        yield ''.join(comb)
+
+
 class TestUrlInit(unittest.TestCase):
     """Url instantiation."""
-
-    @staticmethod
-    def urls_for_test():
-        """URL generator."""
-
-        protocols = ['', 'file:///', 'ftp://', 'http://', 'https://']
-        paths = ['', 'home', 'home/foo', 'home/foo/bar.txt']
-        params = ['', '?abc=cde', '?abc=cde&FgH=iJk!@#$%']
-        randoms = ['', ''.join(random.choices(string.ascii_letters, k=32))]
-
-        for comb in itertools.product(protocols, paths, params, randoms):
-            yield ''.join(comb)
-
 
     def test_attr_url(self):
         """Instantiation with URL preserves received value."""
 
-        for url in self.urls_for_test():
+        for url in urls_for_test():
             with self.subTest(url=url):
                 self.assertEqual(url, urlm.Url(url).url)
 

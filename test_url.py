ccbc6e804dcdd4a1afa93991592f645efa9caa8d
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-18 13:12:47 -0300

    add file test_url.py

---
+++ test_url.py
@@ -0,0 +1,32 @@
+import unittest
+import url
+
+class TestUrl(unittest.TestCase):
+
+    def setUpClass(self):
+        pass
+
+
+    def tearDownClass(self):
+        pass
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_init(self):
+        """New instance has the proper attributes with the proper
+        values."""
+
+        # instantiate with controlled URL
+
+        # test attributes from the instance (including URL)
+
+
+if '__name__' == '__main__':
+    unittest.main()

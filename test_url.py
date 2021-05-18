a9db156053b593ef152ec0ba9ba0ed2b39cfa58d
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-18 16:36:28 -0300

    add generator for test URLs

--- test_url.py
+++ test_url.py
@@ -1,5 +1,10 @@
 import unittest
 import url
+import itertools
+import random
+import string
+import itertools
+
 
 class TestUrl(unittest.TestCase):
 
@@ -17,6 +22,19 @@ class TestUrl(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+
+    @staticmethod
+    def test_urls():
+        """Generate URLs."""
+
+        protocols = ['', 'file:///', 'ftp://', 'http://', 'https://']
+        paths = ['', 'home', 'home/foo', 'home/foo/bar.txt']
+        params = ['', '?abc=cde', '?abc=cde&FgH=iJk!@#$%']
+        randoms = ['', ''.join(random.choices(string.ascii_letters, k=32))]
+
+        for test_url in itertools.product(protocols, paths, params, randoms):
+            yield ''.join(test_url)
 
 
     def test_init(self):

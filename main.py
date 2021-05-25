c67000043799adc19dabff9a1ccd04652893d6da
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-05-25 17:24:01 -0300

    update credentials file name

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import url as urlm
 
 # This variable specifies the name of a file that contains the OAuth 2.0
 # information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "client_secret.json"
+CLIENT_SECRETS_FILE = "client_secrets-web.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
